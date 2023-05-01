cc2e32f8cc8aa6c76196d0b381942c675a3d81de
Eddie-Cohanim <102543623+Eddie-Cohanim@users.noreply.github.com>
2023-05-01 17:26:04 +0300

    1st draft

---
+++ movie.h
@@ -0,0 +1,37 @@
+#include "wet1util.h"
+
+
+
+
+
+class Movie
+{
+    public:
+        Movie(int movieID,int views, bool vipOnly, Genre genre);
+
+        ~Movie() = default;
+
+        void addRating(int rating);
+        
+        void addViews(int views);
+
+        bool getVipOnly();
+
+        int getViews();
+
+        int getMovieId();
+
+        int getRating();
+
+        Genre getGenre();
+
+
+    private:
+        int m_movieId;
+        int m_views;
+        int m_numOfVoters;
+        double m_rating;
+        bool m_vipOnly;
+        Genre m_genre;
+
+};
